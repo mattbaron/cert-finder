d9bca058d99686109e1e0538122d9ec3d206a402
Matt Baron <mwb@unitf.net>
2023-03-04 21:30:51 -0600

    Checkpoint

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/mattbaron/cert-finder
 
 go 1.19
+
+require golang.org/x/crypto v0.6.0 // indirect
--- main.go
+++ main.go
@@ -6,20 +6,32 @@ import (
 	"os"
 
 	"github.com/mattbaron/cert-finder/finder"
+	"golang.org/x/crypto/pkcs12"
 )
+
+func DoPem(File string, Data []byte) {
+	block, _ := pem.Decode(Data)
+	if block != nil {
+		fmt.Printf("OK Pem: %s, type=%v\n", File, block.Type)
+	} else {
+		fmt.Printf("ERR Pem: %s\n", File)
+	}
+}
+
+func DoPkcs12(File string, Data []byte) {
+	_, cert, _ := pkcs12.Decode(Data, "")
+	if cert != nil {
+		fmt.Printf("OK Pem: %s %s\n", File, cert.NotAfter)
+	} else {
+		fmt.Printf("ERR Pem: %s\n", File)
+	}
+}
 
 func main() {
 	finder := finder.NewFinder(".crt", ".cer", ".pem", ".der", ".jks", ".pfx", ".p12", ".cert")
-	for _, file := range finder.FindFiles("/Users/mbaron/google-cloud-sdk") {
-		dat, err := os.ReadFile(file)
-		if err == nil {
-			fmt.Println(file)
-			block, _ := pem.Decode(dat)
-
-			if block != nil {
-				fmt.Printf("Block: type=%v\n", block.Type)
-			}
-
-		}
+	for _, file := range finder.FindFiles("/Users/mbaron") {
+		dat, _ := os.ReadFile(file)
+		DoPem(file, dat)
+		DoPkcs12(file, dat)
 	}
 }
